a9d28384e05781002a36fe73644d7c2f869476a6
imgul <bc210411296mgu@vu.edu.pk>
2023-02-21 23:42:31 +0500

    home page set up

---
+++ resources/views/layout/front.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="cdn.jsdelivr.net/npm/choices.js/public/assets/styles/base.min.css" />
+    <link rel="stylesheet" href="cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet">
+    <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <link rel="stylesheet" href="unpkg.com/leaflet%401.7.1/dist/leaflet.css" />
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('css/vendors.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+    <title>@yield('title')</title>
+</head>
+
+<body class="preloader-visible" data-barba="wrapper">
+    <!-- preloader start -->
+    <div class="preloader js-preloader">
+        <div class="preloader__bg"></div>
+    </div>
+    <!-- preloader end -->
+
+    @yield('content')
+
+    <!-- JavaScript -->
+    <script src="unpkg.com/leaflet%401.7.1/dist/leaflet.js"></script>
+    <script src="{{ asset('js/vendors.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+
+</html>
